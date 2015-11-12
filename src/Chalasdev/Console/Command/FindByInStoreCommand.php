82f584fd547d06153c8b435341346b02fa6cf51f
chalasr <robin.chalas@epitech.eu>
2015-11-12 18:03:05 +0100

    setup doctrine inside command line

--- src/Chalasdev/Console/Command/FindByInStoreCommand.php
+++ src/Chalasdev/Console/Command/FindByInStoreCommand.php
@@ -7,10 +7,10 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\FormatterHelper;
+use Chalasdev\Console\Entity;
 
 class FindByInStoreCommand extends Command
 {
-
    protected function configure()
    {
       $this
@@ -20,16 +20,18 @@ class FindByInStoreCommand extends Command
 
    protected function execute(InputInterface $input, OutputInterface $output)
    {
+       $this->setHelperSet($this->getApplication()->getHelperSet());
        $formatter = new FormatterHelper();
        $style = new OutputFormatterStyle('white', 'blue', array('bold'));
        $output->getFormatter()->setStyle('title', $style);
        $welcome = $formatter->formatBlock("Welcome to chalasdev/console-starter", "title", true);
        $output->writeln(['', $welcome, '', 'This project provide standalone for your deployment workflow, built on top of <comment>capistrano/symfony</comment> rubygem .', 'Created by Robin Chalas - github.com/chalasr']);
 
-       $query = $em->createQuery('SELECT u FROM ps_orders u WHERE u.current_state = 14');
-       $products = $query->getResult();
+       $em = $this->getApplication()->getHelperSet()->get('em')->getEntityManager();
+       $repo = $em->getRepository('Chalasdev\Console\Entity\Product');
+       $products = $repo->findAll();
        foreach ($products as $prod) {
-            $output->writeln($prod);
+            $output->writeln($prod->getName());
        }
    }
 }
--- src/Chalasdev/Console/Entity/Product.php
+++ src/Chalasdev/Console/Entity/Product.php
@@ -3,7 +3,6 @@
 namespace Chalasdev\Console\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Account.
@@ -16,60 +15,18 @@ class Product
     /**
      * @var int
      *
-     * @ORM\Column(name="id_product", type="integer", nullable=false)
+     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $idProduct;
+    private $id;
 
     /**
      * @var int
      *
-     * @ORM\Column(name="id_product_attribute", type="integer", nullable=true)
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
      */
-    protected $idProductAttribute;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id_lang", type="integer", nullable=true)
-     */
-    protected $idLang;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="sku", type="string", length=64, nullable=false)
-     */
-    protected $sku;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="marketplace", type="string", length=255, nullable=false)
-     */
-    protected $marketplace;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="action", type="string", length=1, nullable=false)
-     */
-    protected $action;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="dateadd", type="datetime", nullable=true)
-     */
-    protected $dateAdd;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="dateupd", type="datetime", nullable=true)
-     */
-    protected $dateUpd;
+    protected $name;
 
     /**
      * Get id
@@ -103,207 +60,5 @@ class Product
     public function getName()
     {
         return $this->name;
-    }
-
-    /**
-     * Set state
-     *
-     * @param boolean $state
-     *
-     * @return Product
-     */
-    public function setState($state)
-    {
-        $this->state = $state;
-
-        return $this;
-    }
-
-    /**
-     * Get state
-     *
-     * @return boolean
-     */
-    public function getState()
-    {
-        return $this->state;
-    }
-
-    /**
-     * Get idProduct
-     *
-     * @return integer
-     */
-    public function getIdProduct()
-    {
-        return $this->idProduct;
-    }
-
-    /**
-     * Set idProductAttribute
-     *
-     * @param integer $idProductAttribute
-     *
-     * @return Product
-     */
-    public function setIdProductAttribute($idProductAttribute)
-    {
-        $this->idProductAttribute = $idProductAttribute;
-
-        return $this;
-    }
-
-    /**
-     * Get idProductAttribute
-     *
-     * @return integer
-     */
-    public function getIdProductAttribute()
-    {
-        return $this->idProductAttribute;
-    }
-
-    /**
-     * Set idLang
-     *
-     * @param integer $idLang
-     *
-     * @return Product
-     */
-    public function setIdLang($idLang)
-    {
-        $this->idLang = $idLang;
-
-        return $this;
-    }
-
-    /**
-     * Get idLang
-     *
-     * @return integer
-     */
-    public function getIdLang()
-    {
-        return $this->idLang;
-    }
-
-    /**
-     * Set sku
-     *
-     * @param string $sku
-     *
-     * @return Product
-     */
-    public function setSku($sku)
-    {
-        $this->sku = $sku;
-
-        return $this;
-    }
-
-    /**
-     * Get sku
-     *
-     * @return string
-     */
-    public function getSku()
-    {
-        return $this->sku;
-    }
-
-    /**
-     * Set marketplace
-     *
-     * @param string $marketplace
-     *
-     * @return Product
-     */
-    public function setMarketplace($marketplace)
-    {
-        $this->marketplace = $marketplace;
-
-        return $this;
-    }
-
-    /**
-     * Get marketplace
-     *
-     * @return string
-     */
-    public function getMarketplace()
-    {
-        return $this->marketplace;
-    }
-
-    /**
-     * Set action
-     *
-     * @param string $action
-     *
-     * @return Product
-     */
-    public function setAction($action)
-    {
-        $this->action = $action;
-
-        return $this;
-    }
-
-    /**
-     * Get action
-     *
-     * @return string
-     */
-    public function getAction()
-    {
-        return $this->action;
-    }
-
-    /**
-     * Set dateAdd
-     *
-     * @param \DateTime $dateAdd
-     *
-     * @return Product
-     */
-    public function setDateAdd($dateAdd)
-    {
-        $this->dateAdd = $dateAdd;
-
-        return $this;
-    }
-
-    /**
-     * Get dateAdd
-     *
-     * @return \DateTime
-     */
-    public function getDateAdd()
-    {
-        return $this->dateAdd;
-    }
-
-    /**
-     * Set dateUpd
-     *
-     * @param \DateTime $dateUpd
-     *
-     * @return Product
-     */
-    public function setDateUpd($dateUpd)
-    {
-        $this->dateUpd = $dateUpd;
-
-        return $this;
-    }
-
-    /**
-     * Get dateUpd
-     *
-     * @return \DateTime
-     */
-    public function getDateUpd()
-    {
-        return $this->dateUpd;
     }
 }
